bd4d5fee3f2cf1d164659684b7cfbbf3dab57aad
napsev1 <matasnematas@gmail.com>
2024-02-20 14:24:02 +0200

    content

--- src/app/HomePage/Content.tsx
+++ src/app/HomePage/Content.tsx
@@ -1,7 +1,18 @@
 export default function Content() {
     return (
-        <div className="text-white max-w-screen-md mx-auto">
-            <h1>Content</h1>
+        <div className="mx-40 my-20 flex text-white">
+            <div className=" text-3xl ">
+                <h1 className="text-pink-200">Sveikiname atvykus i TAMO!</h1>
+                <h1>Susipažinkite su TAMO slaptažodžio <br/> saugumo instrukcija!
+                 <br/> ----------------------------------------</h1>
+                <h1 className="text-xl">Kodėl reikia saugoti savo TAMO prisijungimo duomenis? </h1>
+                <br/>
+                <h1 className="text-sm"> Atlikti tai tinkamai yra labai svarbu, nes tik taip galima apsisaugoti<br/> nuo nepageidaujamų pašalinių asmenų prisijungimo ir išvengti nepageidaujamų<br/> veiksmų Jūsų TAMO e. dienyno paskyroje. </h1>
+                <br/>
+                <h1 className="text-sm">Kviečiame susipažinti kokius 4 žingsnius naudinga atlikti norint tai užtikrinti.</h1>
+                <br/>
+                <a className="text-pink-300">Sužinoti daugiau... </a>
+            </div>
         </div>
     )
 }
