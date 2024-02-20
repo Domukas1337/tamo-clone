4960566743cc470cd4f2b09bb8d2cda4d9a6f38e
napsev1 <matasnematas@gmail.com>
2024-02-20 13:52:10 +0200

    color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./HomePage/Navbar";
+import Content from "./HomePage/Content";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Content />
     </main>
   );
 }
