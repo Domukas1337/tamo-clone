83d2a6574e296ddb1f40403eec72f51023fe7f4d
Domukas1337 <domukas1337@gmail.com>
2024-02-20 13:42:56 +0200

    navbr

---
+++ src/app/HomePage/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <div className="bg-black p-6">
+            <div className="text-white max-w-screen-md mx-auto">
+                Navbar
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "./HomePage/Navbar";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
     </main>
   );
 }
