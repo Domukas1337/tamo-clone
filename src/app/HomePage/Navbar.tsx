4f60d825415d1b42747514d21ef7a383016e59ce
Domukas1337 <domukas1337@gmail.com>
2024-02-20 13:56:54 +0200

    navcolor

--- src/app/HomePage/Navbar.tsx
+++ src/app/HomePage/Navbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 export default function Navbar() {
     return (
-        <div className="p-6 shadow-lg">
+        <div className="p-6 shadow-lg bg-accent">
             <div className="max-w-screen-md mx-auto">
                 <Image src="/tamod.png" alt="logo" width={100} height={100} />
             </div>
