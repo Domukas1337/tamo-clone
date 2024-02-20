16cc32c7118991b9fab9b148660501343a91e244
Domukas1337 <domukas1337@gmail.com>
2024-02-20 14:59:05 +0200

    api (404 error)

--- src/app/HomePage/Navbar.tsx
+++ src/app/HomePage/Navbar.tsx
@@ -3,7 +3,7 @@ import NavItems from "./NavbarComponents"
 
 export default function Navbar() {
     return (
-        <div className="p-6 shadow-lg bg-accent">
+        <div className="p-6 shadow-lg bg-accent flex flex-row">
             <div className="max-w-screen-md mx-auto flex flex-row">
                 <Image src="/tamod.png" alt="logo" width={100} height={100} className="border-2 border-blue-300 rounded-md"/>
                 <div className="flex flex-row items-center ml-auto gap-10">
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@
 import { useState, FormEvent } from 'react';
 
 export default function Login() {
-  const [email, setEmail] = useState<string>('');
+  const [name, setName] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
@@ -11,17 +11,17 @@ export default function Login() {
     const response = await fetch('/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ name, password }),
     });
 
     // Handle the response from your API
   }
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col'>
-      <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+    <form onSubmit={handleSubmit} className='flex flex-col text-black'>
+      <input type="text" placeholder='Email' value={name} onChange={(e) => setName(e.target.value)} required />
       <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} required />
-      <button type="submit">Login</button>
+      <button className='text-white' type="submit">Login</button>
     </form>
   );
 }
